6503cfa610eb39e4f8ff82c34963bf9bcc07eadc
David Lopera <dein4267@gmail.com>
2025-04-01 17:03:44 -0500

    subir imagen

--- image_processor.h
+++ image_processor.h
@@ -5,32 +5,32 @@
 
 namespace ImageProcessor {
 
-// Estructura para representar una imagen como matriz tridimensional
-class Image {
-public:
-    int width;
-    int height;
-    int channels;
-    
-    // Matriz tridimensional para los píxeles
-    unsigned char*** pixels;
-    
-    Image();
-    ~Image();
-    
-    // Constructor de copia y operador de asignación (prevenir fugas de memoria)
-    Image(const Image& other);
-    Image& operator=(const Image& other);
-    
-    // Asigna memoria para la matriz tridimensional de píxeles usando new
-    void allocateMemory();
-    
-    // Libera la memoria asignada usando delete
-    void freeMemory();
-    
-    // Devuelve información sobre la imagen
-    std::string getInfo() const;
-};
+    // Estructura para representar una imagen como matriz tridimensional
+    class Image {
+    public:
+        int width;
+        int height;
+        int channels;
+        
+        // Matriz tridimensional para los píxeles
+        unsigned char*** pixels;
+        
+        Image();
+        ~Image();
+        
+        // Constructor de copia y operador de asignación (prevenir fugas de memoria)
+        Image(const Image& other);
+        Image& operator=(const Image& other);
+        
+        // Asigna memoria para la matriz tridimensional de píxeles usando new
+        void allocateMemory();
+        
+        // Libera la memoria asignada usando delete
+        void freeMemory();
+        
+        // Devuelve información sobre la imagen
+        std::string getInfo() const;
+    };
 
 } // namespace ImageProcessor
 
